0493120cb798e4dc5cb9cff0d7a598398894b22f
Gareth Field <field.gareth@gmail.com>
2022-09-01 02:35:38 -0400

    lit

--- src/components/LandingWrap.js
+++ src/components/LandingWrap.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const AppWrap = styled.div`
-	max-width: 500px;
-	width: 100%;
-	margin: 0 auto;
+	width: min(100%, 64rem);
+	margin-inline: auto;
+	background: linear-gradient(var(--pink-900), var(--blue-900));
+	min-height: 100vh;
 `;
 
 export default AppWrap;
